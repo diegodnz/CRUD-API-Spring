ec4cfabdfca71b7c1adae053d85104d230d3b77d
diegodnz <diegodiniz991@gmail.com>
2020-11-01 16:49:22 -0200

    Get methods

--- src/main/java/com/alisson/springcloudpostgre/controller/ContactController.java
+++ src/main/java/com/alisson/springcloudpostgre/controller/ContactController.java
@@ -1,9 +1,16 @@
 package com.alisson.springcloudpostgre.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alisson.springcloudpostgre.repository.*;
+import com.alisson.springcloudpostgre.model.*;
 
 @RestController
 @RequestMapping({"/contacts"})
@@ -13,6 +20,19 @@ public class ContactController {
 	
 	ContactController(ContactRepository repository) {
 		this.repository = repository;
+	}
+	
+	// @RequestMapping(value="/contacts", method=RequestMethod.GET)
+	@GetMapping
+	public List<Contact> getAll() {
+		return repository.findAll();
+	}
+	
+	@GetMapping(path = {"/{id}"})
+	public ResponseEntity<Contact> findById(@PathVariable long id) {
+		return repository.findById(id)
+				.map(record -> ResponseEntity.ok().body(record))
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
