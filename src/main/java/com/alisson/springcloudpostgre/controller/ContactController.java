ebbd6c749e117e783870a20a635b4a0b71080601
diegodnz <diegodiniz991@gmail.com>
2020-11-01 17:08:24 -0200

    Update and Creation methods

--- src/main/java/com/alisson/springcloudpostgre/controller/ContactController.java
+++ src/main/java/com/alisson/springcloudpostgre/controller/ContactController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alisson.springcloudpostgre.repository.*;
-import com.alisson.springcloudpostgre.model.*;
+import com.alisson.springcloudpostgre.model.Contact;
 
 @RestController
 @RequestMapping({"/contacts"})
@@ -22,17 +25,62 @@ public class ContactController {
 		this.repository = repository;
 	}
 	
+	// Retrieve All Contacts
 	// @RequestMapping(value="/contacts", method=RequestMethod.GET)
 	@GetMapping
 	public List<Contact> getAll() {
 		return repository.findAll();
 	}
 	
+	// Retrieve a Contact by Id
 	@GetMapping(path = {"/{id}"})
 	public ResponseEntity<Contact> findById(@PathVariable long id) {
 		return repository.findById(id)
 				.map(record -> ResponseEntity.ok().body(record))
 				.orElse(ResponseEntity.notFound().build());
 	}
+	
+	
+	// Create a new Contact
+	@PostMapping
+	public Contact create(@RequestBody Contact contact) {
+		return repository.save(contact);
+	}
+	/* 
+		
+    "name": "Java",
+    "email": "java@gmail.com",
+    "phone": "(111) 111-1111"
+    
+	 */
+	
+	
+	// Update a Contact
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Contact> update(@PathVariable("id") long id,
+			                              @RequestBody Contact contact) {
+		return repository.findById(id)
+				.map(record -> {
+					record.setName(contact.getName());
+					record.setEmail(contact.getEmail());
+					record.setPhone(contact.getPhone());
+					Contact updated = repository.save(record);
+					return ResponseEntity.ok().body(updated);					
+				}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/com/alisson/springcloudpostgre/model/Contact.java
+++ src/main/java/com/alisson/springcloudpostgre/model/Contact.java
@@ -17,6 +17,30 @@ public class Contact {
 	private String name;
 	private String email;
 	private String phone;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	
 
 }
 
