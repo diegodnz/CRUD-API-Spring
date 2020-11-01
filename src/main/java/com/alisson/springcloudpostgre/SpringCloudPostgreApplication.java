d1a0b0f6171405acf91bc624548fb29414ae8e3d
diegodnz <diegodiniz991@gmail.com>
2020-11-01 01:41:29 -0200

    Projeto criado

---
+++ src/main/java/com/alisson/springcloudpostgre/SpringCloudPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.alisson.springcloudpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudPostgreApplication.class, args);
+	}
+
+}
